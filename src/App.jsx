c6598984234fcbb345159bfbfba9689436bef1e7
Rathod Sahil <sahilrathod222@gmail.com>
2026-05-24 11:13:08 +0530

    Configure GitHub Pages Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import DashboardLayout from '@layouts/DashboardLayout';
 import ProtectedRoute from '@routes/ProtectedRoute';
